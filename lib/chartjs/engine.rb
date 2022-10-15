b343bae6df2a1a9593231ffc1a4e9d6022d31fb2
toba75 <github@mfbougies.com>
2022-10-15 12:47:06 +0200

    Update engine.rb
    initializer pour impormap

--- lib/chartjs/engine.rb
+++ lib/chartjs/engine.rb
@@ -2,6 +2,14 @@ require 'chartjs/chart_helpers'
 
 module Chartjs
   class Engine < Rails::Engine
+     # for importmap
+    initializer "chartjs.importmap" do |app|
+      if defined?(Importmap)
+        app.config.assets.precompile << "chart.js"
+        app.config.assets.precompile << "Chart.bundle.js"
+      end
+    end
+    
     initializer 'chartjs.chart_helpers' do
       if ::Chartjs.no_conflict
         ActionView::Base.send :include, Chartjs::ChartHelpers::Explicit
